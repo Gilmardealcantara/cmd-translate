351c8995b078c181427cd83696c7f0b70b9a05d0
gilmardealcantara <gilmardealcantara@gmail.com>
2016-11-28 14:45:56 -0200

    Add install

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get install dictd 
+/etc/init.d/dictd start 
+apt-get install dict-freedict-eng-por 
+/etc/init.d/dictd restart 
+
+mv ../cmd-translate /usr/local
+ln -s /usr/local/cmd-translate/cmd-translate.sh /usr/local/bin/translate
+
+echo "Install ok, run:\ntranslate\n"
